acc5c7270835020bccd64d27a545eaf50fec1856
Luís Jordão <a21201026@isec.pt>
2020-03-24 11:27:35 +0000

    added accelerometer, ui update

--- app/src/main/java/pt/isec/cubiqua/MainActivity.java
+++ app/src/main/java/pt/isec/cubiqua/MainActivity.java
@@ -9,6 +9,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.location.Location;
 import android.os.Bundle;
@@ -23,18 +25,20 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import java.util.List;
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private int locationRequestCode = 1000;
     private FusedLocationProviderClient mFusedLocationClient;
 
     // XML Elements
     private TextView txtLocation;
+    private TextView txtAccelerometer;
     private TextView txtDeviceList;
 
     private double wayLatitude = 0.0, wayLongitude = 0.0;
 
-    private SensorManager sensorManager;
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,22 +48,60 @@ public class MainActivity extends AppCompatActivity {
         // Find elements from xml view
         this.txtLocation = (TextView) findViewById(R.id.txtLocation);
         this.txtDeviceList = (TextView) findViewById(R.id.txtDeviceList);
+        this.txtAccelerometer = (TextView) findViewById(R.id.txtAccelerometer);
 
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
-        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        if (mAccelerometer == null){
+            Toast.makeText(getApplicationContext(), "No accelerometer!", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    public void onSensorChanged(SensorEvent event){
+        // In this example, alpha is calculated as t / (t + dT),
+        // where t is the low-pass filter's time-constant and
+        // dT is the event delivery rate.
+
+        final float alpha = (float)0.8;
+        float[] gravity = new float[3];
+        float[] linear_acceleration = new float[3];
+        // Isolate the force of gravity with the low-pass filter.
+        gravity[0] = alpha * gravity[0] + (1 - alpha) * event.values[0];
+        gravity[1] = alpha * gravity[1] + (1 - alpha) * event.values[1];
+        gravity[2] = alpha * gravity[2] + (1 - alpha) * event.values[2];
+
+        // Remove the gravity contribution with the high-pass filter.
+        linear_acceleration[0] = event.values[0] - gravity[0];
+        linear_acceleration[1] = event.values[1] - gravity[1];
+        linear_acceleration[2] = event.values[2] - gravity[2];
+
+        txtAccelerometer.setText("X: " + linear_acceleration[0] + "\nY: "+linear_acceleration[1]+"\nZ: "+linear_acceleration[2]);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+
+        // Location permissions
         this.checkPermissions();
+
         this.getSensorList();
+
+        // Accelerometer
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     private void getSensorList(){
-        List<Sensor> deviceSensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
-        txtDeviceList.setText(deviceSensors.toString());
+        List<Sensor> deviceSensors = mSensorManager.getSensorList(Sensor.TYPE_ALL);
+        // txtDeviceList.setText(deviceSensors.toString());
     }
 
     private void checkPermissions(){
@@ -71,6 +113,12 @@ public class MainActivity extends AppCompatActivity {
         } else {
             this.updateCurrentLocation();
         }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
     }
 
     @Override
